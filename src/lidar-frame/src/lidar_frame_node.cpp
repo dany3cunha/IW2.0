af652fc4749c1afec8a2101e14102759e9ab5285
pedrosilva2703 <up201806526@fe.up.pt>
2022-08-24 11:39:22 +0100

    Lidar frame publisher

---
+++ src/lidar-frame/src/lidar_frame_node.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "lidar_frame");
+    ros::NodeHandle node;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+
+    ros::Rate rate(10.0);
+    while (node.ok())
+    {
+        transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
+        transform.setRotation(tf::createQuaternionFromRPY(-M_PI / 2, 0, 0));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "zed2_left_camera_frame", "lidar_frame"));
+        rate.sleep();
+    }
+    return 0;
+};
